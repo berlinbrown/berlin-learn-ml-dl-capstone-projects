2c4b5d507d38c24e7793f3595e452d2deac224ed
Berlin Brown <berlin.brown@gmail.com>
2024-08-17 10:59:34 -0400

    Adding updates for basic ml
    Basic ml

---
+++ basic-exercises/ai-dl-perceptron-py/ml-basic-perceptron.py
@@ -0,0 +1,11 @@
+# Basic perceptron
+# Based on: https://github.com/rasbt/machine-learning-book/blob/main/ch02/ch02.py
+# https://github.com/rasbt/
+# From Sebastian
+# 
+# 
+# # Machine Learning with PyTorch and Scikit-Learn  
+# # -- Code Examples
+
+if __name__ == '__main__':
+    print("Running")
